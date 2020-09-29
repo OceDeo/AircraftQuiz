afa473ee8a5ae37e35d9f1365416c39b302329cc
Lukasz Mildner <lukasz1330@gmail.com>
2020-09-29 15:24:56 +0100

    started html and flask

--- aircraft_scrapdata.py
+++ aircraft_scrapdata.py
@@ -3,20 +3,45 @@ import random
 from bs4 import BeautifulSoup
 import time
 
-i = 0
-limit = 10
 
-while i < limit:
-    number = random.randint(1,9223337)
-    page = requests.get(f'http://jetphotos.com/photo/{str(number)}') 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    title = soup.find('title')
-    if 'new' in page.url:
-        print('photo removed')
-        pass
-    else:
-        aircraft = title.string.split(' | ')[1:2]
+def findplane(aircraft_list):
+    i = 0
+    while i < 1:
+        number = random.randint(1,9223337)
+        page = requests.get(f'http://jetphotos.com/photo/{str(number)}') #
+        soup = BeautifulSoup(page.content, 'html.parser')
+        title = soup.find('title')
+        if 'new' in page.url:
+            pass
+        else:
+            aircraft = title.string.split(' | ')[1:2]
+            manufacturer = aircraft[0].split(' ')[0]
+            try:
+                model = aircraft[0].split(' ', 1)[1]
+            except:
+                model = None
+            if manufacturer in aircraft_list:
+                for model_check in aircraft_list[f'{manufacturer}']:
+                    if model_check in model:
+                        image = soup.find("img", class_ = "large-photo__img")
+                        i += 1
+                        return aircraft[0].split(' ', 1), image['srcset']
+                    else:
+                        pass
+        time.sleep(.1)
+
+
+
+'''def gen_question(aircraft_list):
+    counter = 0
+    lim = 100
+    while counter < lim:
+        aircraft, imagelink = findplane(aircraft_list)
         print(aircraft[0])
-        i += 1
-    time.sleep(1)
+        print(aircraft[1])
+        print(imagelink)
+        counter +=1
+        print(f'----------  {counter}  -------------------')
+    
 
+generate_question(aircraft_list)'''
--- app.py
+++ app.py
@@ -1,2 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, url_for, redirect, g
+import aircraft_scrapdata
 
+app = Flask(__name__)
+
+app.secret_key = 'dYIbV9GL3VtVyGWq2tpjndfuig6d3278gbz6T80WjuY'
+
+@app.route('/question', methods = ['GET', 'POST'])
+def question():
+
+    manufacturer_list = ['Embraer', 'Antonov', "ATR", 'Bombardier', 'Cessna', 'Fokker', 'Tupolev', 'Ilyushin', 'Boeing', 'Airbus']
+
+    aircraft_dict = {
+        'Embraer': ['170', '175', '190', '195', '145', '120'],
+        'Antonov': ['AN-124', 'AN-225'],
+        'ATR': ['ATR-42', 'ATR-72', '72', '42'],
+        'Bombardier': ['Q400', 'CRJ-200', 'CRJ-900', 'CRJ-700' 'Global'],
+        'Cessna': ['152', '172', '182', 'Citation'],
+        'Fokker': ['50', '70', '100'],
+        'Tupolev': ['134'],
+        'Ilyushin': ['62'],
+        'Boeing': ['727', '737', '747', '757', '767', '777', '787'],
+        'Airbus': ['A300', 'A310', 'A318', 'A319', 'A320', 'A321', 'A330', 'A340', 'A350', 'A360']
+        # 'McDonnell Douglas': ['1'],
+        # 'General Dynamics': ['1'],
+        # 'Lockheed Martin': ['1'],
+        # 'British Aerospace': ['1'],
+    }
+
+    aircraft, imagelink = aircraft_scrapdata.findplane(aircraft_dict)
+    manufacturer = aircraft[0]
+    model = aircraft[1]
+    print(manufacturer)
+    if request.method == "POST":
+        manu = request.form['123']
+        print(f'{manu}')
+        return render_template('question.jinja2', manufacturer_list = manufacturer_list, manufacturer = manufacturer)
+    return render_template('question.jinja2', manufacturer_list = manufacturer_list, manufacturer = manufacturer)
+
+if __name__ == '__main__':
+    app.run(debug=True)
